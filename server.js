e24c8c229dd46700de2272a12c0ccc039cf7730c
Andrew Fechner <arfechner@gmail.com>
2017-03-07 16:01:50 -0500

    Working on filepath issues

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ process.env.PWD = process.cwd();
 //mongoose.connect('mongodb://localhost/shortly');
 
 // configure our server with all the middleware and routing
-require(__dirname+'/server/config/middleware.js')(app, express);
-require(__dirname+'/server/config/routes.js')(app, express);
+require(process.env.PWD+'/server/config/middleware.js')(app, express);
+require(process.env.PWD+'/server/config/routes.js')(app, express);
 // start listening to requests on port 8000
 app.listen(8000);
 
